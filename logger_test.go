9101e90db53a30ab7112dc19362068b38fa04362
ksrof <kevinsunercontacto@gmail.com>
2021-12-21 16:07:12 +0100

    chore: updated tests methods

--- logger_test.go
+++ logger_test.go
@@ -5,13 +5,16 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"runtime"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 )
 
-// TestStartLog checks whether or not the log file has been created
-func TestStartLog(t *testing.T) {
-	err := StartLog()
+// TestStartLogger checks whether or not the log file has been created
+func TestStartLogger(t *testing.T) {
+	err := StartLogger()
 	if err != nil {
 		t.Fatalf("\n❌ StartLog failed: %v\n", err)
 	}
@@ -33,12 +36,12 @@ func TestStartLog(t *testing.T) {
 		t.Fatalf("\n❌ Unable to match log file: %v\n", err)
 	}
 
-	t.Log("\n✅ StartLog test passed...\n")
+	t.Log("\n✅ StartLogger test passed...\n")
 }
 
-// TestFindLog checks whether or not the log file is found
-func TestFindLog(t *testing.T) {
-	logFile, err := FindLog()
+// TestFindLogger checks whether or not the log file is found
+func TestFindLogger(t *testing.T) {
+	loggerFile, err := FindLogger()
 	if err != nil {
 		t.Fatalf("\n❌ FindLog failed: %v\n", err)
 	}
@@ -48,18 +51,30 @@ func TestFindLog(t *testing.T) {
 		t.Fatalf("\n❌ Unable to get working directory: %v\n", err)
 	}
 
-	if logFile != path.Join(dir, "teavet.log") {
+	if loggerFile != path.Join(dir, "teavet.log") {
 		t.Fatalf("\n❌ Unable to match log file: %v\n", err)
 	}
 
 	t.Log("\n✅ FindLog test passed...\n")
 }
 
-// TestBuildLog checks whether or not the log file has content
-func TestBuildLog(t *testing.T) {
-	logContent, err := BuildLog(&Optional{Status: "success", Message: "testing", Error: nil})
+// TestSaveLogger checks whether or not the log file has content
+func TestSaveLogger(t *testing.T) {
+	// Get information about the file and the line
+	_, filename, line, _ := runtime.Caller(1)
+
+	logger := &LoggerContent{
+		Filename:      filename,
+		Line:          strconv.Itoa(line),
+		Timestamp:     time.Now().Format(time.RFC3339),
+		LoggerStatus:  &LoggerStatus{Status: "Success"},
+		LoggerMessage: &LoggerMessage{Message: "Teavet is up and running!"},
+		LoggerFault:   &LoggerFault{},
+	}
+
+	err := SaveLogger(logger)
 	if err != nil {
-		t.Fatalf("\n❌ BuildLog failed: %v\n", err)
+		t.Fatalf("\n❌ Unable to save logger to log file: %v", err)
 	}
 
 	dir, err := os.Getwd()
@@ -78,16 +93,17 @@ func TestBuildLog(t *testing.T) {
 		t.Fatalf("\n❌ Unable to unmarshal log file content: %v\n", err)
 	}
 
-	if content["filename"] != logContent.Filename {
+	if content["filename"] != logger.Filename {
 		t.Fatalf("\n❌ Unable to match log filename: %v\n", err)
 	}
 
 	t.Log("\n✅ BuildLog test passed...\n")
 }
 
-// TestLogger returns the output of Logger
+// TestLogger returns the output of Complete logger method
 func TestLogger(t *testing.T) {
-	Logger("success", "testing logger", nil)
+	// Complete log type
+	Complete("success", "Teavet is up and running!", nil)
 
 	dir, err := os.Getwd()
 	if err != nil {
@@ -99,4 +115,6 @@ func TestLogger(t *testing.T) {
 	if err != nil {
 		t.Fatalf("\n❌ Unable to remove log file: %v\n", err)
 	}
+
+	t.Log("\n✅ TestLogger test passed...\n")
 }
